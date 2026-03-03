3131c0fcb71065a546428c9a8ee90939e78b71d2
Leticia Gabriele da conceição <lgconceicao@uniara.edu.br>
2026-03-03 19:45:42 -0300

    Aula 04 POO
    Aqui tratamos Exceções

---
+++ proj04/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+try {
+        System.out.println("Digite um numero: ");
+        int a = sc.nextInt();
+
+        System.out.println("O valor digitado é: "+ a );
+
+        System.out.println("Digite um outro numero ");
+        int b = sc.nextInt();
+
+        int resultado = a/b;
+        System.out.println("Resultado:  " + resultado);
+
+
+    }catch (ArithmeticException e){
+    System.out.println("Erro: Não dá para dividir por zero! " );
+    }catch (Exception ex){
+    System.out.println("Erro: voce precisa inserir apenas numeros");
+    }
+        }
+            }
+
